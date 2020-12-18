f39aa3302655ce556adc0e5ac15e536943008ed5
Ahmed <Ahmed@192.168.1.2>
2020-12-18 19:55:56 +0200

    read

--- EmailClient/src/main/java/oop/emailApp/EmailClient/services/FileMethods.java
+++ EmailClient/src/main/java/oop/emailApp/EmailClient/services/FileMethods.java
@@ -1,23 +1,63 @@
 package oop.emailApp.EmailClient.services;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
+import org.json.*;
+
+import oop.emailApp.EmailClient.model.*;
 
 public class FileMethods {
-	
-	public void createDirectory (String filePath) {
-		try {
-		    Path path = Paths.get(filePath);
-		    Files.createDirectories(path);
 
-		  } catch (Exception e) {
-		    
-		  }
-		
+	public void appendMailToFile(String filePath, Mail mail) {
+
 	}
-	public void appendMFile (String filePath, String Content) {
-		
+
+	public void appendContactToFile(String filePath, Contact contact) {
+		try {
+			JSONArray jsonArray;
+			File f1 = new File(filePath);
+			if (!f1.exists()) {
+				f1.createNewFile();
+				jsonArray = new JSONArray();
+			} else {
+				jsonArray = new JSONArray(ReadFromFile(filePath));
+			}
+			JSONObject obj = new JSONObject("{\"email\":\"" + contact.getEmail() + "\",\"name\":\"" + contact.getName()
+					+ "\",\"password\":\"" + contact.getPassword() + "\"}");
+			jsonArray.put(obj);
+			FileWriter fileWriter = new FileWriter(f1.getName(), true);
+			BufferedWriter bw = new BufferedWriter(fileWriter);
+			bw.write(jsonArray.toString());
+			bw.close();
+
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	public String ReadFromFile(String FilePath) {
+		StringBuilder str = new StringBuilder();
+		try {
+			@SuppressWarnings("resource")
+			BufferedReader reader = new BufferedReader(new FileReader(FilePath));
+			String line = null;
+			String ls = System.getProperty("line.separator");
+
+			while ((line = reader.readLine()) != null) {
+				str.append(line);
+				str.append(ls);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		str.deleteCharAt(str.length() - 1);
+		return str.toString();
 	}
 
 }
