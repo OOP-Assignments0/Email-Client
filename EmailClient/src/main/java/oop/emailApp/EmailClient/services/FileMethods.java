aab4e31b23285421ae97d446989e34e17bef1ea6
Ahmed <Ahmed@192.168.1.2>
2020-12-18 20:07:04 +0200

    append json object

--- EmailClient/src/main/java/oop/emailApp/EmailClient/services/FileMethods.java
+++ EmailClient/src/main/java/oop/emailApp/EmailClient/services/FileMethods.java
@@ -13,11 +13,9 @@ import oop.emailApp.EmailClient.model.*;
 
 public class FileMethods {
 
-	public void appendMailToFile(String filePath, Mail mail) {
-
-	}
-
-	public void appendContactToFile(String filePath, Contact contact) {
+	
+	
+	public void appendJsonObjectToFile(String filePath, JSONObject obj) {
 		try {
 			JSONArray jsonArray;
 			File f1 = new File(filePath);
@@ -27,8 +25,6 @@ public class FileMethods {
 			} else {
 				jsonArray = new JSONArray(ReadFromFile(filePath));
 			}
-			JSONObject obj = new JSONObject("{\"email\":\"" + contact.getEmail() + "\",\"name\":\"" + contact.getName()
-					+ "\",\"password\":\"" + contact.getPassword() + "\"}");
 			jsonArray.put(obj);
 			FileWriter fileWriter = new FileWriter(f1.getName(), true);
 			BufferedWriter bw = new BufferedWriter(fileWriter);
@@ -36,7 +32,7 @@ public class FileMethods {
 			bw.close();
 
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		}
 
 	}
@@ -59,5 +55,34 @@ public class FileMethods {
 		str.deleteCharAt(str.length() - 1);
 		return str.toString();
 	}
+	
+	public void appendToFile() {
+		
+	}
+
+	/*public void appendContactToFile(String filePath, Contact contact) {
+		try {
+			JSONArray jsonArray;
+			File f1 = new File(filePath);
+			if (!f1.exists()) {
+				f1.createNewFile();
+				jsonArray = new JSONArray();
+			} else {
+				jsonArray = new JSONArray(ReadFromFile(filePath));
+			}
+			JSONObject obj = new JSONObject("{\"email\":\"" + contact.getEmail() + "\",\"name\":\"" + contact.getName()
+					+ "\",\"password\":\"" + contact.getPassword() + "\"}");
+			jsonArray.put(obj);
+			FileWriter fileWriter = new FileWriter(f1.getName(), true);
+			BufferedWriter bw = new BufferedWriter(fileWriter);
+			bw.write(jsonArray.toString());
+			bw.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}*/
+
 
 }
