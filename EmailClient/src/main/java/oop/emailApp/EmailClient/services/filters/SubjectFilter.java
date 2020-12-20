98e54ba6677d3f467dbc96e1b10d323a6961e899
ahmed <ahmed@192.168.1.7>
2020-12-20 09:28:42 +0200

    filter

--- EmailClient/src/main/java/oop/emailApp/EmailClient/services/filters/SubjectFilter.java
+++ EmailClient/src/main/java/oop/emailApp/EmailClient/services/filters/SubjectFilter.java
@@ -22,21 +22,20 @@ public class SubjectFilter extends Filter {
 	public ArrayList<Mail> meetFilter(ArrayList<Mail> list, String Word) {
 		ArrayList<Mail> Subjectlist = new ArrayList<Mail>();
 		for (Mail m : list) {
-			if (checkWord(m.getSubject(), Word)) {
+			if (m.getSubject().contains(Word)) {
 				Subjectlist.add(m);
 			}
 		}
 		return Subjectlist;
 	}
 
-	protected boolean checkWord(String Subject, String Word) {
-		/*String[] words = Subject.split("\\s+");
+	/*protected boolean checkWord(String Subject, String Word) {
+		String[] words = Subject.split("\\s+");
 		for (String w : words) {
 			if (w.equalsIgnoreCase(Word)) {
 				return true;
 			}
-		}*/
-		if (Subject.contains(Word)) return true;
+		}
 		return false;
-	}
+	}*/
 }
