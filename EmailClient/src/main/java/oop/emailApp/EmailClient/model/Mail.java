06e4a2dd0c74d1632ed02135c26fa535297f8b5e
Ahmed <Ahmed@My-HP>
2020-12-21 21:04:09 +0200

    update mail

--- EmailClient/src/main/java/oop/emailApp/EmailClient/model/Mail.java
+++ EmailClient/src/main/java/oop/emailApp/EmailClient/model/Mail.java
@@ -1,7 +1,5 @@
 package oop.emailApp.EmailClient.model;
 
-import java.util.ArrayList;
-
 import org.json.JSONObject;
 
 
@@ -11,8 +9,7 @@ public class Mail  {
 	private String to;
 	private String subject;
 	private String date;
-	private ArrayList<Object> attachments = new ArrayList<Object>();
-	private String attachlinks[];
+	private String[] attachments;
 	private int priority;
     private String name;
     private String folder;
@@ -29,11 +26,8 @@ public class Mail  {
 	public void setBody(String email) {
 		this.body = email;
 	}
-	public void setAttachments(ArrayList<Object> attachment) {
-		this.attachments = attachment;
-	}
-	public void setAttaclinks(String attach[]) {
-		this.attachlinks=attach;
+	public void setAttachments(String[] attachment) {
+		this.attachments = attachment.clone();
 	}
 	public void setPriority(int priority) {
 		this.priority = priority;
@@ -60,11 +54,8 @@ public class Mail  {
 	public String getBody() {
 		return body;
 	}
-	public ArrayList<Object> getAttachments() {
+	public String[] getAttachments() {
 		return attachments;
-	}
-	public String[] getAttaclinks() {
-		return attachlinks;
 	}
 	public int getPriority() {
 		return priority;
@@ -80,8 +71,7 @@ public class Mail  {
 	}
 	public Mail copy() {
 		Mail m = new Mail();
-		m.attachments =(this.attachments);
-		m.attachlinks = this.attachlinks;
+		m.attachments = this.attachments;
 		m.body = this.body;
 		m.from = this.from;
 		m.to = this.to;
@@ -95,6 +85,7 @@ public class Mail  {
 
 	public JSONObject dataToString() {
 		JSONObject jsonObject=new JSONObject();
+		
 		jsonObject.put("from",this.from);
         jsonObject.put("to",this.to);
         jsonObject.put("priority",this.priority);
@@ -103,6 +94,7 @@ public class Mail  {
         jsonObject.put("date",this.date);
         jsonObject.put("body",this.body);
         jsonObject.put("folder", this.body);
+        
         
 		return jsonObject;
         
