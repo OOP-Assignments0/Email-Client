7235b1e4ce1fab497fdcaf0cd3e343dbd32e9005
ahmed <ahmed@192.168.1.7>
2020-12-23 21:23:26 +0200

    update

--- EmailClient/src/main/java/oop/emailApp/EmailClient/EmailClientApplication.java
+++ EmailClient/src/main/java/oop/emailApp/EmailClient/EmailClientApplication.java
@@ -10,10 +10,6 @@ public class EmailClientApplication {
 
 	public static void main(String[] args) {
 		Method.loadContacts();
-		
-		/*for (int i = 0 ; i < Contact.getContacts().size();i++) {
-			System.out.println(Contact.getContacts().get(i).ContactTOJsonObject().toString());
-		}*/
 		SpringApplication.run(EmailClientApplication.class, args);
 	}
 
